cb7c1dcc74a159cf43efed77e9b28091ccc66585
Ray Vafa <vafatabar@gmail.com>
2019-02-10 21:47:11 +1100

    cleanup

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
   app.listen(port);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  if (swaggerExpress.runner.swagger.paths['/api/user']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/api/user');
   }
 });
